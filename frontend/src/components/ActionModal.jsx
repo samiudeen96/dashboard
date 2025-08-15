26e4c2dbb4008d3004ec2826165a7b740d4e1395
Samiudeen2021 <sami@hasnatech.onmicrosoft.com>
2025-08-15 18:03:55 +0400

    update

--- frontend/src/components/ActionModal.jsx
+++ frontend/src/components/ActionModal.jsx
@@ -12,9 +12,9 @@ const ActionModal = () => {
     }
     closeModal();
   };
-
+ 
   return (
-    <div className="fixed inset-0 bg-black/40 flex justify-center items-center z-50">
+    <div className="fixed inset-0 bg-black/40 flex justify-center items-center z-50" onClick={closeModal}>
       <div className="bg-white rounded-sm shadow-sm m-3 p-5 min-w-[300px]">
         <p>{modalData.desc}</p>
         <div className="flex justify-center gap-5 mt-5">
